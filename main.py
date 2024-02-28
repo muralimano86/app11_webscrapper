e5f664b24864c52e39072c829e09e4c4cd80cb34
murali <muralimano86@gmail.com>
2024-02-28 19:03:48 +0000

    Storing the extracted data in text file

--- main.py
+++ main.py
@@ -16,7 +16,27 @@ def extract(source):
     return value
 
 
+def send_email():
+    print("Email was sent:")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read():
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scrapped = scrape(URL)
     extracted = extract(scrapped)
     print(extracted)
+    content = read()
+
+    if extracted not in "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
